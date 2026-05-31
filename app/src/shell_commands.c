f5fb648ec24e1d8edacb98014667496a042b8b10
Enzo Donadel <enzo.donadel95@gmail.com>
2026-05-31 10:48:54 -0300

    added sensor set command

--- app/src/shell_commands.c
+++ app/src/shell_commands.c
@@ -1,6 +1,8 @@
+#include <stdlib.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/shell/shell.h>
+#include "led_sensor.h"
 
 #if CONFIG_LED_SENSOR
 
@@ -34,10 +36,27 @@ static int cmd_sensor_info(const struct shell *sh, size_t argc, char **argv)
 	return 0;
 }
 
+static int cmd_sensor_set(const struct shell *sh, size_t argc, char **argv)
+{
+	char *end;
+	long val = strtol(argv[1], &end, 10);
+
+	if (*end != '\0' || (val != 0 && val != 1)) {
+		shell_error(sh, "Invalid value '%s': expected 0 or 1", argv[1]);
+		return -EINVAL;
+	}
+
+	int ret = led_sensor_set_state(led_sensor_dev, (bool)val);
+
+	shell_print(sh, "led_sensor_set_state(%ld) returned %d", val, ret);
+	return ret;
+}
+
 SHELL_STATIC_SUBCMD_SET_CREATE(sensor_cmds,
 	SHELL_CMD(fetch, NULL, "Call sensor_sample_fetch()", cmd_sensor_fetch),
 	SHELL_CMD(read,  NULL, "Call sensor_channel_get() and print result", cmd_sensor_read),
 	SHELL_CMD(info,  NULL, "Print device name and ready state", cmd_sensor_info),
+	SHELL_CMD_ARG(set, NULL, "Set LED state: sensor set <0|1>", cmd_sensor_set, 2, 0),
 	SHELL_SUBCMD_SET_END
 );
 
